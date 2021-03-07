fe9958b9c88ffa6bd3ca0c9ffaf96f50db893bba
Brecht Bekaert <brecht@bekaert.cloud>
2021-03-07 13:02:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-
-# Tor relay entrypoint
-# Copyright (C) 2017-2018 Rodrigo Martínez <dev@brunneis.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 if [ "$(whoami)" == "root" ]; then
   # Fix if the container is launched with the root (host) user
   if [ $HOST_UID -eq 0 ]; then
